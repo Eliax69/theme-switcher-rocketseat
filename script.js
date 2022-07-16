1a461e4e4c3640ed2b3b6a0be22e347dccf93cc8
Elias Mendes <eliaxem69@gmail.com>
2022-07-15 21:54:31 -0300

    finalização do projeto.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+var body = document.querySelector('body')
 var button = document.querySelector('.btn-switch')
 
 button.addEventListener('click', () => {
     button.classList.contains('dark') ? (
         button.classList.remove('dark'),
-        button.classList.add('white')
+        button.classList.add('white'),
+        body.style.backgroundColor = '#F1F1F1'
     ) : (
         button.classList.remove('white'),
-        button.classList.add('dark')
+        button.classList.add('dark'),
+        body.style.backgroundColor = '#292C35'
     )
 })
